6b4e2d9ba0f81ca3ed6965e3c10fb14351a6c305
Tony Guntharp <fusion94@damagestudios.net>
2014-03-19 06:39:18 -0700

    testing some boxen bits

---
+++ modules/people/manifests/fusion94.pp
@@ -0,0 +1,4 @@
+class people::fusion94 {
+  ## Declare all subclasses
+  include people::fusion94::applications
+}
---
+++ modules/people/manifests/fusion94/applications.pp
@@ -0,0 +1,7 @@
+class people::fusion94::applications {
+  include atom
+  include iterm2::stable
+  include onepassword
+  include dropbox
+  include chrome
+}
