1d788bb77574560527d00ec6c428b4f38af2b6ef
Tony Guntharp <fusion94@damagestudios.net>
2014-03-19 16:34:54 -0700

    removing zsh

--- modules/people/manifests/fusion94.pp
+++ modules/people/manifests/fusion94.pp
@@ -2,17 +2,4 @@ class people::fusion94 {
   ## Declare all subclasses
   include people::fusion94::applications
 
-  # Changes the default shell to the zsh version we get from Homebrew
-  # Uses the osx_chsh type out of boxen/puppet-osx
-  osx_chsh { $::luser:
-    shell   => '/opt/boxen/homebrew/bin/zsh',
-    require => Package['zsh'],
-  }
-
-  file_line { 'add zsh to /etc/shells':
-    path    => '/etc/shells',
-    line    => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh'],
-  }
-
 }
